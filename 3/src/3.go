fa9ef79f3abdf9641b01bff3ec738061c8986698
IliaFeldgun <ilia.feldgun@zadarastorage.com>
2024-12-04 00:02:32 +0200

    3-p2

--- 3/src/3.go
+++ 3/src/3.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"os"
 	"regexp"
@@ -9,23 +8,38 @@ import (
 )
 
 func parseValues() {
+  dontr, _ := regexp.Compile(`don't\(\)`)
+  dor, _ := regexp.Compile(`do\(\)`)
   input_path := os.Args[1]
-  file, err := os.Open(input_path)
+  binary_content, err := os.ReadFile(input_path)
   if err != nil {
     log.Fatal(err)
   }
-  r, _ := regexp.Compile(`mul\([0-9]+,[0-9]+\)`)
-  lineScanner := bufio.NewScanner(file)
-  lineScanner.Split(bufio.ScanLines)
+
   sum := 0
-  for lineScanner.Scan(){
-    line_match := r.FindAllString(lineScanner.Text(), -1)
-    for _, mul := range line_match {
-      lfactor, rfactor := getFactors(mul)
-      sum += lfactor * rfactor
+  split_by_dont := dontr.Split(string(binary_content), -1)
+  sum += getMulSum(split_by_dont[0])
+  for i := 1; i < len(split_by_dont); i++ {
+    split_by_do := dor.Split(split_by_dont[i], 2)
+    if len(split_by_do) > 1 {
+      log.Print(sum, " ", split_by_dont[i])
+      sum += getMulSum(split_by_do[1])
+      log.Print(sum, " ", split_by_do[1], " ", split_by_do[0])
+      log.Print(sum)
     }
   }
   log.Print(sum)
+}
+
+func getMulSum(safe_mul_string string) (int) {
+  mulr, _ := regexp.Compile(`mul\([0-9]+,[0-9]+\)`)
+  line_match := mulr.FindAllString(safe_mul_string, -1)
+  sum := 0
+  for _, mul := range line_match {
+    lfactor, rfactor := getFactors(mul)
+    sum += lfactor * rfactor
+  }
+  return sum
 }
 
 func getFactors(mul_directive string) (int, int) {
