542cdad8dbb9f4ba201c1c042d664ee0e7ebe4ae
IliaFeldgun <ilia.feldgun@zadarastorage.com>
2024-12-14 07:43:23 +0200

    10-p2

--- 10/src/10.go
+++ 10/src/10.go
@@ -20,27 +20,28 @@ func parseValues() {
 	height := len(bin_matrix)
 	width := len(bin_matrix[0])
 	log.Print(string(bytes.Join(bin_matrix, []byte("\n"))))
-	paths := make(map[Location]map[Location]bool)
+	paths := make(map[Location]map[Location]int)
 	for row := 0; row < height; row++ {
 		for col := 0; col < width; col++ {
 			if bin_matrix[row][col] == []byte("0")[0] {
 				this_location := Location{row, col}
 				this_neighbors := getNextNeighbors(bin_matrix, this_location)
-				this_neighbors[this_location] = true
 				paths[Location{row, col}] = this_neighbors
 			}
 		}
 	}
 	log.Print(paths)
 	sum := 0
+	rsum := 0
 	for l, v := range paths {
 		nine_score := 0
-
+		rating := 0
 		if bin_matrix[l.row][l.col] == []byte("0")[0] {
 			// if bin_matrix[l.row][l.col] == []byte("0")[0] &&
 			// 	l.row == 0 || l.col == 0 || l.row == len(bin_matrix)-1 || l.col == len(bin_matrix[0])-1 {
 			for i := range v {
 				if bin_matrix[i.row][i.col] == []byte("9")[0] {
+					rating += v[i]
 					nine_score++
 				}
 			}
@@ -49,10 +50,12 @@ func parseValues() {
 			sum += nine_score
 			printPath2(v, bin_matrix)
 			log.Print(nine_score)
+			rsum += rating
 		}
 	}
 	log.Print(len(paths))
 	log.Print(sum)
+	log.Print(rsum)
 	// for i := []byte("0")[0]; i < []byte("9")[0]; i++ {
 	// 	height_locations
 	// 	for j := []byte("0")[0]; j < []byte("9")[0]; j++ {
@@ -74,7 +77,7 @@ func printPath(path []Location, bin_matrix [][]byte) {
 	log.Print(output)
 }
 
-func printPath2(path map[Location]bool, bin_matrix [][]byte) {
+func printPath2(path map[Location]int, bin_matrix [][]byte) {
 	output_matrix := allocate_matrix(bytes.Join(bin_matrix, []byte("\n")))
 	for row := range output_matrix {
 		for col := range output_matrix[row] {
@@ -87,17 +90,24 @@ func printPath2(path map[Location]bool, bin_matrix [][]byte) {
 	log.Print("\n", string(bytes.Join(output_matrix, []byte("\n"))))
 }
 
-func getNextNeighbors(bin_matrix [][]byte, location Location) map[Location]bool {
+func getNextNeighbors(bin_matrix [][]byte, location Location) map[Location]int {
+	next_neighbors := map[Location]int{}
 	if bin_matrix[location.row][location.col] == []byte("9")[0] {
-		return map[Location]bool{location: true}
+		next_neighbors[location] = 0
+		return next_neighbors
 	}
 	neighbors := getNeighbors(bin_matrix, location)
-	next_neighbors := map[Location]bool{}
-	for _, l := range neighbors {
-		if bin_matrix[location.row][location.col] == bin_matrix[l.row][l.col]-1 {
-			next_neighbors[l] = true
-			for k, v := range getNextNeighbors(bin_matrix, l) {
-				next_neighbors[k] = v
+	for _, nloc := range neighbors {
+		if bin_matrix[location.row][location.col] == bin_matrix[nloc.row][nloc.col]-1 {
+			next_neighbors[location] += 1
+			_, ok := next_neighbors[nloc]
+			if !ok {
+				next_neighbors[nloc] = 0
+			}
+			for k, v := range getNextNeighbors(bin_matrix, nloc) {
+				next_neighbors[k] += v
+				next_neighbors[location] += v + 1
+				next_neighbors[nloc] += v + 1
 			}
 		}
 	}
